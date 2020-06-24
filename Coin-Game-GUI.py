a2a06786cd97db9c56032eddf94ead10b93bc92a
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-24 17:35:05 +0200

    did test and made bug fixes

--- Coin-Game-GUI.py
+++ Coin-Game-GUI.py
@@ -2,36 +2,42 @@ from tkinter import *
 import random
 
 root = Tk()
+root.geometry("400x400")
 
-def cmd():
+def cmd(event):
     num = random.randint(1, 3)
+    e.delete(0, END)
 
-    if num == 1 and e.get().lower() == heads:
+    if num == 1 and e.get().lower() == "heads":
         lbl.config(text="[Heads] You betted right")
     
-    if num == 1 and e.get().lower() != heads:
+    if num == 1 and e.get().lower() != "heads":
         lbl.config(text="[Heads] You betted wrong")
 
-    if num == 2 and e.get().lower() == heads:
+    if num == 2 and e.get().lower() == "tails":
         lbl.config(text="[Tails] You betted right")
     
-    if num == 2 and e.get().lower() != heads:
+    if num == 2 and e.get().lower() != "tails":
         lbl.config(text="[Tails] You betted wrong")
 
 def game():
     global lbl, e
 
     done = 1
-
     
     e = Entry(root)
-    e.pack
+    e.pack()
+    e.bind("<Return>", cmd)
 
-    btn = Button(root, command=cmd, text="Submit Bet").pack()
+#    btn = Button(root, command=cmd, text="Submit Bet").pack()
+
+    lbl = Label(root, text"Put bet in box (heads/tails)")
 
     lbl = Label(root, text="")
     lbl.pack()
 
     done -= 1
+
+game()
 
 mainloop()
