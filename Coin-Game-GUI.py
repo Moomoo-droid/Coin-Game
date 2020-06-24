0a2c5d5ef5437421ffb5d6393c38004b616cb97c
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-24 17:21:48 +0200

    Adding interactables and new features

--- Coin-Game-GUI.py
+++ Coin-Game-GUI.py
@@ -1,9 +1,25 @@
 from tkinter import *
 import random
 
+root = Tk()
+
+def cmd():
+    num = random.randint(1, 3)
+
+    if num == 1:
+        lbl.config(text="[Heads]")
+
 def game():
+    global lbl, e
 
     done = 1
 
     while done:
-        e = Entry
+        e = Entry(root)
+        e.pack
+
+        btn = Button(root, command=cmd, text="Submit Bet").pack()
+
+        lbl = Label(root, text="")
+
+mainloop()
