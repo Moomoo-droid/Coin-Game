0a671ebf72273092a9f2df17ee5da5c80e4f231e
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-24 17:25:33 +0200

    Made the program functional

--- Coin-Game-GUI.py
+++ Coin-Game-GUI.py
@@ -6,8 +6,17 @@ root = Tk()
 def cmd():
     num = random.randint(1, 3)
 
-    if num == 1:
-        lbl.config(text="[Heads]")
+    if num == 1 and e.get().lower() == heads:
+        lbl.config(text="[Heads] You betted right")
+    
+    if num == 1 and e.get().lower() != heads:
+        lbl.config(text="[Heads] You betted wrong")
+
+    if num == 2 and e.get().lower() == heads:
+        lbl.config(text="[Tails] You betted right")
+    
+    if num == 2 and e.get().lower() != heads:
+        lbl.config(text="[Tails] You betted wrong")
 
 def game():
     global lbl, e
@@ -21,5 +30,6 @@ def game():
         btn = Button(root, command=cmd, text="Submit Bet").pack()
 
         lbl = Label(root, text="")
+        lbl.pack()
 
 mainloop()
