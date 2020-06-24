04cad81208704fdd7892207ef50634cc45d86120
Moomoo-droid <mattgaming2010@gmail.com>
2020-06-24 17:49:47 +0200

    finished bug fixing

--- Coin-Game-GUI.py
+++ Coin-Game-GUI.py
@@ -5,36 +5,36 @@ root = Tk()
 root.geometry("400x400")
 
 def cmd(event):
-    num = random.randint(1, 3)
+    num = random.randint(1, 2)
     e.delete(0, END)
 
     if num == 1 and e.get().lower() == "heads":
-        lbl.config(text="[Heads] You betted right")
+        lbl2.config(text="[Heads] You betted right")
     
-    if num == 1 and e.get().lower() != "heads":
-        lbl.config(text="[Heads] You betted wrong")
+    if num == 1 and e.get().lower() != "tails":
+        lbl2.config(text="[Heads] You betted wrong")
 
     if num == 2 and e.get().lower() == "tails":
-        lbl.config(text="[Tails] You betted right")
+        lbl2.config(text="[Tails] You betted right")
     
-    if num == 2 and e.get().lower() != "tails":
-        lbl.config(text="[Tails] You betted wrong")
+    if num == 2 and e.get().lower() != "heads":
+        lbl2.config(text="[Tails] You betted wrong")
 
 def game():
-    global lbl, e
+    global lbl2, e
 
     done = 1
-    
+
+    lbl = Label(root, text="Put bet in box (heads/tails)").pack()
+
     e = Entry(root)
     e.pack()
     e.bind("<Return>", cmd)
 
 #    btn = Button(root, command=cmd, text="Submit Bet").pack()
 
-    lbl = Label(root, text"Put bet in box (heads/tails)")
-
-    lbl = Label(root, text="")
-    lbl.pack()
+    lbl2 = Label(root, text="")
+    lbl2.pack()
 
     done -= 1
 
